3415c154e10530c2c4b14b8ffb1dbec735b94a44
Klyukay Evangelina <klyukay.evangelina@gmail.com>
2021-02-21 21:43:06 +0300

    Model

--- NYTimes_API/Model/Article.swift
+++ NYTimes_API/Model/Article.swift
@@ -7,26 +7,78 @@
 
 import Foundation
 
-struct Article: Codable {
-    let url: String
-    let publishedDate: String
-    let byline: String
+class Article: NSObject {
+
     let title: String
-    let section: String
-    let media:[Media]
+    let abstract: String
+    let byline: String
+    let publishedDate: Date?
+    let url: URL?
+
+    private let multimediaItems: [Multimedia]
     
-    struct Media: Codable {
-        let caption: String
-        let mediaMetadata: [MediaMetadata]
+    init(data: [String: Any]) {
+        self.title = data["title"] as? String ?? ""
+        self.abstract = data["abstract"] as? String ?? ""
+        self.byline = data["byline"] as? String ?? ""
         
-        struct MediaMetadata: Codable {
-            let url: String
-            let format: String
+        if let urlString = data["url"] as? String {
+            self.url = URL(string: urlString)
+        } else {
+            self.url = nil
         }
         
-        enum CodingKeys: String, CodingKey {
-            case mediaMetadata = "media-metadata"
-            case caption
+        if let multimediaData = data["multimedia"] as? [[String: Any]] {
+            self.multimediaItems = multimediaData.map({Multimedia(data: $0)})
+        } else {
+            self.multimediaItems = []
+        }
+
+        if let dateString = data["published_date"] as? String {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+
+            self.publishedDate = dateFormatter.date(from: dateString)
+        } else {
+            self.publishedDate = nil
+        }
+    }
+    
+    func getMultimedia(maxWidth: Int, maxHeight: Int) -> Multimedia? {
+        var resultMultimedia: Multimedia? = nil
+        
+        for multimedia in multimediaItems {
+            if (multimedia.height > maxHeight || multimedia.width > maxWidth) {
+                continue
+            }
+            
+            if resultMultimedia == nil {
+                resultMultimedia = multimedia
+            } else if (multimedia.height > resultMultimedia!.height && multimedia.width > resultMultimedia!.width) {
+                resultMultimedia = multimedia
+            }
+        }
+        
+        return resultMultimedia
+    }
+    
+}
+
+class Multimedia: NSObject {
+    
+    let width: Int
+    let height: Int
+    let url: URL?
+
+    init(data: [String: Any]) {
+        
+        width = data["width"] as? Int ?? 0
+        height = data["height"] as? Int ?? 0
+        
+        if let urlString = data["url"] as? String {
+            self.url = URL(string: urlString)
+        } else {
+            self.url = nil
         }
     }
 }
