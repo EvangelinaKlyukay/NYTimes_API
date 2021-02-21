0c1af0e878d5beb50f4cc325ece90043d8494e38
Klyukay Evangelina <klyukay.evangelina@gmail.com>
2021-02-21 21:42:54 +0300

    API сервисы

--- NYTimes_API/Network/APIService.swift
+++ NYTimes_API/Network/APIService.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import Alamofire
+
+class APIService {
+
+    let api_key = "tGfkijceXaIxcSsmKYMkMAFrbxl9lFOb"
+
+    func getArticles(completion: @escaping ([Article]) -> Void, onFail: ((Error) -> Void)?) {
+
+        AF.request("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=\(api_key)").responseJSON { response in
+            switch response.result {
+            case .success(let data):
+                guard let json = data as? [String: Any], let results = json["results"] as? [[String: Any]] else {
+                    print("Unable to parse response")
+                    return
+                }
+
+                let articles = results.map({ Article(data: $0)})
+                completion(articles)
+
+            case .failure(let error):
+                print(error.localizedDescription)
+                onFail?(error)
+            }
+        }
+    }
+}
--- NYTimes_API/Network/WebImageView.swift
+++ NYTimes_API/Network/WebImageView.swift
@@ -5,4 +5,17 @@
 //  Created by Евангелина Клюкай on 19.02.2021.
 //
 
-import Foundation
+import UIKit
+
+class WebImageView: UIImageView {
+    func load(url: URL) {
+        let task = URLSession.shared.dataTask(with: url) { (data, respons, error) in
+            let image = UIImage(data: data!)
+            DispatchQueue.main.async {
+                self.image = image
+            }
+        }
+        task.resume()
+    }
+    
+}
